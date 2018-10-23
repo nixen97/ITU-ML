90128b88a3746ea9a53c67ed78c371ef2fa907d0
Nicolaj Filrup Rasmussen <nixen97@gmail.com>
2018-10-23 14:21:28 +0200

    Bit of work on 8.1

--- week8/81.py
+++ week8/81.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
 import matplotlib.pyplot as plt
+import numpy as np
+
+def Pr():
+    pass
+
 
 # Read in data
 data = []
@@ -12,3 +17,23 @@ with open("faithful.csv", "r") as f:
 # Looks like it could be two gaussians
 plt.hist(data)
 #plt.show()
+
+k = 2 # Assume two distributions
+
+# Initialization
+# TODO: Make this K-means
+
+means = [0] * k
+sds   = [1] * k
+pis   = [10] * k
+
+means[0] = 1.32
+means[1] = 0.32
+
+
+#EM Algorithm
+#Init Q
+
+
+
+
