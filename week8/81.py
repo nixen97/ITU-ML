bd99be99e90a1a9f7d51efa6d45934b0303e8423
Nicolaj Filrup Rasmussen <nixen97@gmail.com>
2018-10-24 14:51:58 +0200

    Made 8.1 more better

--- week8/81.py
+++ week8/81.py
@@ -2,16 +2,28 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
-def dnorm(x, mu, sigma):
-    return (1/(sigma * np.sqrt(2 * np.pi)) * np.exp(-(x - mu)**2 / (2 * sigma**2) ))
-
+# Math function
 def prob1d(x, mean, std):
     frac = 1 / (std * np.sqrt(2 * np.pi))
     power = -0.5 * ((x - mean) / std)**2
 
     return frac * np.exp(power)
 
+def Pr(x, means, sds, pis, n, k):
+    res = 0
+    for i in range(n):
+        intermres = 0
+        for j in range(k):
+            temp = pis[j] * prob1d(x[i], means[j], sds[j])
+            intermres += 0 if np.isinf(intermres) else temp
+        if intermres == 0:
+            raise ZeroDivisionError()
+        res += np.log(intermres)
+    return res
+
+# Printing function
 def printndarray(text, arr):
     fs = text
     n = len(arr)
@@ -23,7 +35,7 @@ def printndarray(text, arr):
 def sum_not_inf(arr):
     arr = np.array(list(arr))
     return np.nansum(arr[np.isinf(arr) == False])
-    
+
 
 # Read in data
 with open("faithful.csv", "r") as f:
@@ -32,36 +44,32 @@ with open("faithful.csv", "r") as f:
 
 # Histogram
 # Looks like it could be two gaussians
-plt.hist(data, bins=15, density=True)
+fig, (ax1, ax2) = plt.subplots(1, 2)
+ax2.hist(data, bins=15, density=True)
 #plt.show()
 
-k = 2 # Assume two distributions
+k = 2 # Number of clusters; SHOULD be able to take any value. May not converge though
 n = len(data)
-it = 20
+max_iter = 1000
 
 # Initialization
-# TODO: Make this K-means
+# TODO: k-means initialization
 
 means = np.zeros(k, dtype=float)
 sds   = np.ones(k, dtype=float)
-pis   = np.ones(k, dtype=float)
+pis   = np.repeat(1/n, k)
 taus  = np.zeros(k, dtype=float)
 
-pis[0] = 0.5
-pis[1] = 0.5
+means = np.array(np.random.randint(1, 6, k), dtype=float)
 
-means[0] = 1.7
-means[1] = 4.4
+sds = np.array(np.random.randint(1, 2, k), dtype=float)
 
-sds[0] = 0.5
-sds[1] = 1.5
+# EM Algorithm
 
-#EM Algorithm
+# Log likelihood vector
+Q = np.zeros(max_iter, dtype=float)
 
-#Log likelihood vector
-Q = np.zeros(it, dtype=float)
-
-for L in range(it):
+for L in range(max_iter):
     # E step
     respons = np.zeros((k, n))
     
@@ -75,8 +83,6 @@ for L in range(it):
     respons[np.isinf(respons)] = 0.5
     respons = respons / np.sum(respons,axis=0)
 
-    #print(respons)
-    #exit(0)
     # M step
     nc = np.zeros(k)
     for i in range(n):
@@ -95,62 +101,32 @@ for L in range(it):
     sds /= nc
     sds = np.sqrt(sds)
 
+    #Log likelihood for iteration
+    Q[L] = Pr(data, means, sds, pis, n, k)
+
+    if any(np.isnan(means)) or any(np.isnan(sds)):
+        exit(0) # We done fucked up
+
     print("\n*****\n")
     print("L = %d" % (L+1))
     printndarray("Nc's: ", nc)
     printndarray("Pi's: ", pis)
     printndarray("Means: ", means)
     printndarray("Sigmas: ", sds)
+    print("Log likelihood: %f" % (Q[L]))
+    print("Log likelihood delta: %f" % (np.abs(Q[L-1] - Q[L])))
 
-    # TODO: Compute the log likelihood and add an exit condition to exit when
-    # log likelihood change is very small.
-
-    if any(np.isnan(means)) or any(np.isnan(sds)):
-        exit(0) # We done fucked up
+    if np.abs(Q[L-1] - Q[L]) < 0.000001:
+        break
 
 
 x = np.linspace(1, 5.5, 100)
-y = pis[0] * prob1d(x, means[0], sds[0]) + pis[1] * prob1d(x, means[1], sds[1])
+y = np.sum(np.array(list(map(lambda i: pis[i] * prob1d(x, means[i], sds[i]), range(k)))), axis=0)
 
-plt.plot(x, y, color='red', linewidth=2)
+y_cum = np.cumsum(y)
+
+ax2.plot(x, y, color='red', linewidth=2)
+ax1.plot(x, y_cum, color='blue', linewidth=2)
 plt.show()
 
-# Compute for L = 0 and L = 1
-# Q[0] = 0
-# Q[1] = sum_not_inf(map(lambda i: sum_not_inf(pis[i] * (np.log(pis[i]) + np.log(dnorm(data, means[i], sds[i])))), range(k)))
-
-# L = 1
-
-# while np.abs(Q[L]-Q[L-1]) > 0.000001 and L < 1001:
-#     # Intermediate value
-#     taus = np.array(list(map(lambda i: np.array(pis[i] * dnorm(data, means[i], sds[i]) / \
-#         sum(map(lambda i: pis[i] * dnorm(data, means[i], sds[i]), range(k)))), range(k))), dtype=float)
-
-#     taus[np.isinf(taus)] = 0.5
-
-#     tau_sums = np.array(list(map(sum_not_inf, taus)))
-    
-#     # New pis
-#     pis = tau_sums / len(data)
-    
-#     # New means
-#     means = list(map(lambda i: sum_not_inf(taus[i] * data) / tau_sums[i], range(k)))
-
-#     # New SD's
-#     sds = np.array(list(map(lambda i: sum_not_inf(taus[i] * (data - means[i])**2) / tau_sums[i], range(k))), dtype=float)
-
-#     # New Log likelihood
-#     Q[L+1] = sum(map(lambda i: sum_not_inf(taus[i] * (np.log(pis[i]) + np.log(dnorm(data, means[i], sds[i])))), range(k)))
-    
-#     # Print some stats
-#     print("\n*****\n")
-#     print("L = %d" % (L+1))
-#     printndarray("Pi's: ", pis)
-#     printndarray("Means: ", means)
-#     printndarray("Sigmas: ", sds)
-#     print("Log likelihood: %f" % (Q[L+1]))
-#     print("Delta log likelihood: %f" % (np.abs(Q[L]-Q[L+1])))
-
-#     L += 1
-    
     
